84712f9ca35fa137d63ce91460385caf23607c34
coder432 <avinashmkamath@gmail.com>
2016-10-09 13:58:17 +0530

    Update web_cam_face.py

--- web_cam_face.py
+++ web_cam_face.py
@@ -3,7 +3,8 @@
 import cv2
 
 #path for cascades
-casc_path_face=' enter cascade path here' #copy the location of face cascade as string eg(C:\opencv\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml)
+casc_path_face=' enter cascade path here' 
+#copy the location of face cascade as string eg(C:\opencv\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml)
 
 #creating cascades
 face_cascade=cv2.CascadeClassifier(casc_path_face)
